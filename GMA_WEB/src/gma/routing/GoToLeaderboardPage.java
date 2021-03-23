32e93bc71c37581aae59a2b284a86dc9adc3dd80
Enrico Massaro <emassaro98@users.noreply.github.com>
2021-03-23 22:26:43 +0100

    Fix
    - fix

---
+++ GMA_WEB/src/gma/routing/GoToLeaderboardPage.java
@@ -0,0 +1,72 @@
+package gma.routing;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import gma.services.*;
+import gma.entities.*;
+
+@WebServlet("/Leaderboard")
+public class GoToLeaderboardPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	/*@EJB(name = "it.polimi.db2.mission.services/MissionService")
+	private MissionService mService;
+	@EJB(name = "it.polimi.db2.mission.services/ProjectService")
+	private ProjectService pService;*/
+
+	public GoToLeaderboardPage() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// If the user is not logged in (not present in session) redirect to the login
+		String loginpath = getServletContext().getContextPath() + "/index.html";
+		HttpSession session = request.getSession();
+		if (session.isNew() || session.getAttribute("user") == null) {
+			response.sendRedirect(loginpath);
+			return;
+		}
+
+		User user = (User) session.getAttribute("user");
+		// Redirect to the Home page and add missions to the parameters
+		String path = "/WEB-INF/leaderboard.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void destroy() {
+	}
+
+}
