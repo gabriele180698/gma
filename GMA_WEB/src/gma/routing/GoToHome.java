d4119490b500739450158f5ef76d786837ab9f84
Enrico Massaro <emassaro98@users.noreply.github.com>
2021-03-23 22:21:37 +0100

    Routing, controllers and pages
    - create pages for admin and user;
    - create relative servelts;
    - create logout controller;
    - start to develop the home page;

---
+++ GMA_WEB/src/gma/routing/GoToHome.java
@@ -0,0 +1,82 @@
+package gma.routing;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import gma.services.*;
+import gma.entities.*;
+
+@WebServlet("/Home")
+public class GoToHome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	@EJB(name = "gma.services/ProductService")
+	private ProductService pService;
+
+	public GoToHome() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// If the user is not logged in (not present in session) redirect to the login
+		String loginpath = getServletContext().getContextPath() + "/index.html";
+		HttpSession session = request.getSession();
+		if (session.isNew() || session.getAttribute("user") == null) {
+			response.sendRedirect(loginpath);
+			return;
+		}
+
+		User user = (User) session.getAttribute("user");
+		// Redirect to the Home page and add missions to the parameters
+		String path = "/WEB-INF/home.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		Product product = null;
+	
+		//Get the needed data
+		try {
+			//Get the product of the day
+			product = pService.getProductOfDay();
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to get data");
+			return;
+		}
+		
+		//Pass data to the page
+		ctx.setVariable("product", product);
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void destroy() {
+	}
+
+}
