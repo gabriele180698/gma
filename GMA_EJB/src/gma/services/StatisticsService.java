327b162a12bf7526caf081e40af31121d6f26af8
gabriele180698 <58036457+gabriele180698@users.noreply.github.com>
2021-03-27 17:44:25 +0100

    Sending Statistics functionality
    SubmitStatistic implementation
    CancelQuestionnaire implementation
    QuestionnairePage now contains the optional questions

--- GMA_EJB/src/gma/services/StatisticsService.java
+++ GMA_EJB/src/gma/services/StatisticsService.java
@@ -16,19 +16,58 @@ public class StatisticsService {
 	public StatisticsService() {
 	}
 
-	public void submitStatistics(int score, int age, int expertise, int idQuestionnaire, int sex, User user,
-			int status) {
+	public void submitStatistics(int age, int expertise, int idQuestionnaire, int sex, User user) {
 		Questionnaire questionnaire;
 		Statistics stat;
+		int score;
+		
 		stat = new Statistics();
 		questionnaire = em.createNamedQuery("Questionnaire.findQuestionnaireById", Questionnaire.class)
-				.setParameter(1, idQuestionnaire).getSingleResult();
-		stat.setScore(score);
+				.setParameter(1, 2).getSingleResult();
+		score = 0;
+		
+
+		if(age!=0) {
+			score += 2;
+		}
 		stat.setAge(age);
-		stat.setExpertise(expertise);
+
+		// Expertise level: 0 = no info; 1 = low; 2 = medium; 3 = high;
+		if(expertise!=0) {
+			score += 2;
+			stat.setExpertise(expertise);
+		}
+		
+		// Status = 0 = cancelled; 1 = submitted
+		stat.setStatus(1);
+		
+		// Sex: 0 = no info; 1 = female; 2 = male; 3 = other;
+		if(sex!=0) {
+			score += 2;
+			stat.setSex(sex);
+		}
+		
+		stat.setScore(score);
 		stat.setQuestionnaire(questionnaire);
-		stat.setSex(sex);
 		stat.setUser(user);
-		stat.setStatus(status);
+		em.persist(stat);
+	}
+	
+	
+	public void cancelStatistics(int idQuestionnaire, User user) {
+		Questionnaire questionnaire;
+		Statistics stat;
+		int score;
+		
+		stat = new Statistics();
+		questionnaire = em.createNamedQuery("Questionnaire.findQuestionnaireById", Questionnaire.class)
+				.setParameter(1, 2).getSingleResult();
+		
+		// Status = 0 = cancelled; 1 = submitted
+		stat.setStatus(0);
+
+		stat.setQuestionnaire(questionnaire);
+		stat.setUser(user);
+		em.persist(stat);
 	}
 }
