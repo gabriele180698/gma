ef14ed5c12d8a858bfe1a216fb320afac98f5386
vanessatamma <vanessatamma@users.noreply.github.com>
2021-03-28 22:22:38 +0200

    create questionnaire

--- GMA_WEB/src/gma/objects/Paths.java
+++ GMA_WEB/src/gma/objects/Paths.java
@@ -2,7 +2,7 @@ package gma.objects;
 
 public enum Paths {
 	INDEX_PAGE("/index.html"), USER_HOME_PAGE("/WEB-INF/home.html"), ADMIN_HOME_PAGE("/WEB-INF/admin/adminPanel.html"), QUESTIONNAIRE_PAGE("/WEB-INF/questionnaire.html"),
-	LEADERBOARD_PAGE("/WEB-INF/leaderboard.html"), THANKS_PAGE("/WEB-INF/thanks.html"), ADMIN_HOME("/Admin/Home"), USER_HOME("/App/Home");
+	LEADERBOARD_PAGE("/WEB-INF/leaderboard.html"), THANKS_PAGE("/WEB-INF/thanks.html"), ADMIN_HOME("/Admin/Home"), USER_HOME("/App/Home"), CREATE_QUESTIONNAIRE_PAGE("/WEB-INF/admin/createQuestionnaire.html");
 	
 	private final String path;
 	
